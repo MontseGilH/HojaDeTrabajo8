3ec32e080cd9dca471f5bef2b31c2ae005296bd1
montsegh4545 <80225989+montsegh4545@users.noreply.github.com>
2022-04-06 22:15:42 -0600

    base del programa

---
+++ PriorityQueueI.java
@@ -0,0 +1,5 @@
+
+
+public interface PriorityQueueI<T> {
+
+}
---
+++ QueueFactory.java
@@ -0,0 +1,11 @@
+import java.util.PriorityQueue;
+
+public class QueueFactory<E> {
+    public PriorityQueueI<E> getQueue(String entry){
+        if (entry.equalsIgnoreCase("PQ")){
+            
+        } else{
+
+        }
+    }
+}
