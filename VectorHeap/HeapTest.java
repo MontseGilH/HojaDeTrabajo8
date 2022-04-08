564332e882e327e59616c1837f01e5936e03647b
montsegh4545 <80225989+montsegh4545@users.noreply.github.com>
2022-04-07 20:43:44 -0600

    pruebas unitarias

--- VectorHeap/HeapTest.java
+++ VectorHeap/HeapTest.java
@@ -1,3 +1,28 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
 public class HeapTest {
-    
+	/**
+	 * Evalua agregar a un arbol
+	 */
+	@Test
+	void testInsert()  {
+		VectorHeap<String> pac =  new VectorHeap<String>();
+		pac.add("hola");
+		pac.add("adios");
+		pac.add("roca");
+		pac.add("perfume");
+		pac.add("spotify");
+		try {
+			assertEquals("adios",pac.remove());
+			assertEquals("hola",pac.remove());
+			assertEquals("perfume",pac.remove());
+			assertEquals("roca",pac.remove());
+			assertEquals("spotify",pac.remove());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 }
